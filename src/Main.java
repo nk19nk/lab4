082b0c76d2754e03e775dac6eda87f59283780ef
nasty <nastya_aa19@mail.ru>
2024-12-02 20:56:10 +0500

    1

--- src/Main.java
+++ src/Main.java
@@ -32,86 +32,91 @@ public class Main {
         return res;
     }
 
-
+    //метод для задачи 1.1
+    public static void method(Box<Integer> box) {
+        try {
+            Integer value = box.getObj();
+            System.out.println("Из коробки извлечено: " + value);
+        } catch (Exception e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+    }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
         //1.1
         System.out.println("Задание 1.1");
-//        Box<Integer> box = new Box<>();
-//        System.out.println("Введите целое число для добавления в коробку:");
-//        int x;
-//        while (true) {
-//            if (in.hasNextInt()) {
-//                x = in.nextInt();
-//                break;
-//            } else {
-//                System.out.println("Введите целое число:");
-//                in.next();
-//            }
-//        }
-//        try {
-//            box.setObj(x);
-//            System.out.println("Добавление прошло успешно");
-//        } catch (Exception e) {
-//            System.out.println("Ошибка: " + e.getMessage());
-//        }
-//        System.out.println(box.toString());
-//        method(box);
-//        System.out.println(box.toString());
-
+        Box<Integer> box = new Box<>();
+        System.out.println("Введите целое число для добавления в коробку:");
+        int n;
+        while (true) {
+            if (in.hasNextInt()) {
+                n = in.nextInt();
+                break;
+            } else {
+                System.out.println("Введите целое число:");
+                in.next();
+            }
+        }
+        try {
+            box.setObj(n);
+            System.out.println("Добавление прошло успешно");
+        } catch (Exception e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+        System.out.println(box.toString());
+        method(box);
+        System.out.println(box.toString());
 
         //2.2
-//        System.out.println("Задание 2.2");
-//        List<Box<Integer>> boxes = new ArrayList<>();
-//        System.out.println("Введите количество коробок:");
-//        int kol;
-//        while (true) {
-//            if (in.hasNextInt()) {
-//                kol = in.nextInt();
-//                if (kol > 0) {
-//                    break;
-//                } else {
-//                    System.out.println("Введите положительное число:");
-//                }
-//            } else {
-//                System.out.println("Введите целое число:");
-//                in.next();
-//            }
-//        }
-//        for (int i = 1; i <= kol; i++) {
-//            Box<Integer> box = new Box<>();
-//            System.out.println("Введите целое число для коробки " + i + ":");
-//            while (true) {
-//                if (in.hasNextInt()) {
-//                    int value = in.nextInt();
-//                    try {
-//                        box.setObj(value);
-//                        System.out.println("Число " + value + " добавлено в коробку");
-//                        break;
-//                    } catch (Exception e) {
-//                        System.out.println("Ошибка: " + e.getMessage());
-//                    }
-//                } else {
-//                    System.out.println("Введите корректное целое число:");
-//                    in.next();
-//                }
-//            }
-//            boxes.add(box);
-//        }
-//
-//        System.out.println("Введенные коробоки:");
-//        for (int i = 0; i < boxes.size(); i++) {
-//            System.out.println((i + 1) + ") " + boxes.get(i).toString());
-//        }
-//
-//        try {
-//            double max = maxdouble(boxes);
-//            System.out.println("Максимальное значение: " + max);
-//        } catch (Exception e) {
-//            System.out.println(e.getMessage());
-//        }
+        System.out.println("Задание 2.2");
+        List<Box<Integer>> boxes = new ArrayList<>();
+        System.out.println("Введите количество коробок:");
+        int kol;
+        while (true) {
+            if (in.hasNextInt()) {
+                kol = in.nextInt();
+                if (kol > 0) {
+                    break;
+                } else {
+                    System.out.println("Введите положительное число:");
+                }
+            } else {
+                System.out.println("Введите целое число:");
+                in.next();
+            }
+        }
+        for (int i = 1; i <= kol; i++) {
+            Box<Integer> box2 = new Box<>();
+            System.out.println("Введите целое число для коробки " + i + ":");
+            while (true) {
+                if (in.hasNextInt()) {
+                    int value = in.nextInt();
+                    try {
+                        box2.setObj(value);
+                        System.out.println("Число " + value + " добавлено в коробку");
+                        break;
+                    } catch (Exception e) {
+                        System.out.println("Ошибка: " + e.getMessage());
+                    }
+                } else {
+                    System.out.println("Введите корректное целое число:");
+                    in.next();
+                }
+            }
+            boxes.add(box2);
+        }
+        System.out.println("Введенные коробоки:");
+        for (int i = 0; i < boxes.size(); i++) {
+            System.out.println((i + 1) + ") " + boxes.get(i).toString());
+        }
+        try {
+            double max = maxdouble(boxes);
+            System.out.println("Максимальное значение: " + max);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
         System.out.println("Задание 3.1");
         System.out.println("Преобразование списка строк в список их длин");
@@ -134,16 +139,5 @@ public class Main {
         }
         List<Integer> maxValues = method1(arr, x -> Arrays.stream(x).max().orElse(0));
         System.out.println("Максимальные значения: " + maxValues);
-    }
-
-
-    //метод для задачи 1.1
-    public static void method(Box<Integer> box) {
-        try {
-            Integer value = box.getObj();
-            System.out.println("Из коробки извлечено: " + value);
-        } catch (Exception e) {
-            System.out.println("Ошибка: " + e.getMessage());
-        }
     }
 }
